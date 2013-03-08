8221e6b59afee41a5282cd1e61c59c01690511a6
JasWSInc <jason@websharks-inc.com>
2013-03-07 23:55:39 -0900

    Several developmental changes.
    - Too many to list. Still in development.

--- websharks-core-v000000-dev/classes/websharks-core-v000000-dev/videos.php
+++ websharks-core-v000000-dev/classes/websharks-core-v000000-dev/videos.php
@@ -10,158 +10,151 @@
  * @since 120318
  */
 namespace websharks_core_v000000_dev
-{
-	if(!defined('WPINC'))
-		exit('Do NOT access this file directly: '.basename(__FILE__));
-
-	/**
-	 * Videos.
-	 *
-	 * @package WebSharks\Core
-	 * @since 120318
-	 *
-	 * @assert ($GLOBALS[__NAMESPACE__])
-	 */
-	class videos extends framework
 	{
-		/**
-		 * Builds a URL to a YouTube® video.
-		 *
-		 * @param string $video The video ID.
-		 * @param array  $args Optional array of arguments that may choose to override our defaults.
-		 *
-		 * @return string URL to a YouTube® video.
-		 *
-		 * @throws exception If invalid types are passed through arguments list.
-		 */
-		public function yt_url($video, $args = array())
-		{
-			$this->check_arg_types('string:!empty', 'array', func_get_args());
-
-			$url      = $this->©url->current_scheme().'://www.youtube.com/v/'.urlencode($video);
-			$defaults = array(
-				'version'        => 2,
-				'hd'             => 1,
-				'fs'             => 1,
-				'rel'            => 0,
-				'autoplay'       => 0,
-				'start'          => 0,
-				'showinfo'       => 0,
-				'modestbranding' => 1,
-				'hl'             => 'en_US',
-				'theme'          => 'light'
-			);
-			$args     = array_merge($defaults, $args);
-			$args     = $this->©string->ify_deep($args);
-
-			return add_query_arg(urlencode_deep($args), $url);
-		}
+		if(!defined('WPINC'))
+			exit('Do NOT access this file directly: '.basename(__FILE__));
 
 		/**
-		 * Builds an embed tag for a YouTube® video.
+		 * Videos.
 		 *
-		 * @param string  $video The video ID.
-		 * @param array   $args Optional array of arguments that may choose to override our defaults.
-		 * @param array   $classes Optional array of CSS classes for the embed tag.
-		 * @param string  $attrs Optional string of additional attributes for the embed tag.
+		 * @package WebSharks\Core
+		 * @since 120318
 		 *
-		 * @return string Embed tag for a YouTube® video.
-		 *
-		 * @throws exception If invalid types are passed through arguments list.
+		 * @assert ($GLOBALS[__NAMESPACE__])
 		 */
-		public function yt_embed_tag($video, $args = array(), $classes = array(), $attrs = '')
+		class videos extends framework
 		{
-			$this->check_arg_types('string:!empty', 'array', 'array', 'string', func_get_args());
+			/**
+			 * YouTube® video (IFRAME URL).
+			 *
+			 * @param string $video The video ID.
+			 * @param array  $args Optional arguments.
+			 *
+			 * @return string IFRAME URL to a YouTube® video.
+			 *
+			 * @throws exception If invalid types are passed through arguments list.
+			 */
+			public function yt_iframe_url($video, $args = array())
+				{
+					$this->check_arg_types('string:!empty', 'array', func_get_args());
 
-			$defaults = array(
-				'width'  => '98%',
-				'height' => '98%'
-			);
-			$args     = array_merge($defaults, $args);
-			$args     = $this->©string->ify_deep($args);
+					$url      = $this->©url->current_scheme().'://www.youtube.com/embed/'.urlencode($video);
+					$defaults = array(
+						'rel'         => 0,
+						'enablejsapi' => 1,
+						'playerapiid' => $video,
+						'origin'      => $this->©url->current_host()
+					);
+					$args     = array_merge($defaults, $args);
+					$args     = $this->©string->ify_deep($args);
 
-			return '<embed type="application/x-shockwave-flash"'.
+					return add_query_arg(urlencode_deep($args), $url);
+				}
 
-			       ' src="'.esc_attr($this->yt_url($video, $args)).'"'.
-			       ' wmode="transparent" allowscriptaccess="always" allowfullscreen="true"'.
+			/**
+			 * YouTube® video (IFRAME tag).
+			 *
+			 * @param string $video The video ID.
+			 * @param array  $args Optional array of arguments.
+			 * @param array  $classes Optional array of CSS classes for the IFRAME tag.
+			 * @param string $attrs Optional string of additional attributes for the IFRAME tag.
+			 *
+			 * @return string IFRAME tag for a YouTube® video.
+			 *
+			 * @throws exception If invalid types are passed through arguments list.
+			 *
+			 * @see https://developers.google.com/youtube/player_parameters
+			 */
+			public function yt_iframe_tag($video, $args = array(), $classes = array(), $attrs = '')
+				{
+					$this->check_arg_types('string:!empty', 'array', 'array', 'string', func_get_args());
 
-			       (($classes) ? ' class="'.esc_attr(implode(' ', $classes)).'"' : '').
+					$defaults = array(
+						'width'  => '98%',
+						'height' => '98%'
+					);
+					$args     = array_merge($defaults, $args);
+					$args     = $this->©string->ify_deep($args);
 
-			       ' style="width:'.esc_attr($args['width']).'; height:'.esc_attr($args['height']).';'.
-			       ' padding:1%; background:#CCCCCC; border-radius:3px;"'.
+					return '<iframe type="text/html" frameborder="0"'.
 
-			       (($attrs) ? ' '.$attrs : '').
-			       ' />';
-		}
+					       ' src="'.esc_attr($this->yt_iframe_url($video, $args)).'"'.
 
-		/**
-		 * Builds a URL to a YouTube® video playlist.
-		 *
-		 * @param string $playlist The playlist ID.
-		 * @param array  $args Optional array of arguments that may choose to override our defaults.
-		 *
-		 * @return string URL to a YouTube® video playlist.
-		 *
-		 * @throws exception If invalid types are passed through arguments list.
-		 */
-		public function yt_playlist_url($playlist, $args = array())
-		{
-			$this->check_arg_types('string:!empty', 'array', func_get_args());
+					       (($classes) ? ' class="'.esc_attr(implode(' ', $classes)).'"' : '').
 
-			$url      = $this->©url->current_scheme().'://www.youtube.com/p/'.urlencode($playlist);
-			$defaults = array(
-				'version'        => 2,
-				'hd'             => 1,
-				'fs'             => 1,
-				'rel'            => 0,
-				'autoplay'       => 0,
-				'start'          => 0,
-				'showinfo'       => 0,
-				'modestbranding' => 1,
-				'hl'             => 'en_US',
-				'theme'          => 'light'
-			);
-			$args     = array_merge($defaults, $args);
-			$args     = $this->©string->ify_deep($args);
+					       ' style="width:'.esc_attr($args['width']).'; height:'.esc_attr($args['height']).';'.
+					       ' padding:1%; background:#CCCCCC; border-radius:3px;"'.
 
-			return add_query_arg(urlencode_deep($args), $url);
-		}
+					       (($attrs) ? ' '.$attrs : '').
+					       '></iframe>';
+				}
 
-		/**
-		 * Builds an embed tag for a YouTube® video playlist.
-		 *
-		 * @param string $playlist The playlist ID.
-		 * @param array  $args Optional array of arguments that may choose to override our defaults.
-		 * @param array  $classes Optional array of CSS classes for the embed tag.
-		 * @param string $attrs Optional string of additional attributes for the embed tag.
-		 *
-		 * @return string Embed tag for a YouTube® video playlist.
-		 *
-		 * @throws exception If invalid types are passed through arguments list.
-		 */
-		public function yt_playlist_embed_tag($playlist, $args = array(), $classes = array(), $attrs = '')
-		{
-			$this->check_arg_types('string:!empty', 'array', 'array', 'string', func_get_args());
+			/**
+			 * YouTube® playlist (IFRAME URL).
+			 *
+			 * @param string $playlist The playlist ID.
+			 * @param array  $args Optional array of arguments.
+			 *
+			 * @return string IFRAME URL to a YouTube® video playlist.
+			 *
+			 * @throws exception If invalid types are passed through arguments list.
+			 */
+			public function yt_playlist_iframe_url($playlist, $args = array())
+				{
+					$this->check_arg_types('string:!empty', 'array', func_get_args());
 
-			$defaults = array(
-				'width'  => '98%',
-				'height' => '98%'
-			);
-			$args     = array_merge($defaults, $args);
-			$args     = $this->©string->ify_deep($args);
+					$url      = $this->©url->current_scheme().'://www.youtube.com/embed';
+					$playlist = 'PL'.preg_replace('/^PL/', '', $playlist);
+					$defaults = array(
+						'rel'         => 0,
+						'enablejsapi' => 1,
+						'playerapiid' => $playlist,
+						'origin'      => $this->©url->current_host(),
+						'listType'    => 'playlist',
+						'list'        => $playlist
+					);
+					$args     = array_merge($defaults, $args);
+					$args     = $this->©string->ify_deep($args);
 
-			return '<embed type="application/x-shockwave-flash"'.
+					return add_query_arg(urlencode_deep($args), $url);
+				}
 
-			       ' src="'.esc_attr($this->yt_playlist_url($playlist, $args)).'"'.
-			       ' wmode="transparent" allowscriptaccess="always" allowfullscreen="true"'.
+			/**
+			 * YouTube® playlist (IFRAME tag).
+			 *
+			 * @param string $playlist The playlist ID.
+			 * @param array  $args Optional array of arguments.
+			 * @param array  $classes Optional array of CSS classes for the IFRAME tag.
+			 * @param string $attrs Optional string of additional attributes for the IFRAME tag.
+			 *
+			 * @return string IFRAME tag for a YouTube® video playlist.
+			 *
+			 * @throws exception If invalid types are passed through arguments list.
+			 *
+			 * @see https://developers.google.com/youtube/player_parameters
+			 */
+			public function yt_playlist_iframe_tag($playlist, $args = array(), $classes = array(), $attrs = '')
+				{
+					$this->check_arg_types('string:!empty', 'array', 'array', 'string', func_get_args());
 
-			       (($classes) ? ' class="'.esc_attr(implode(' ', $classes)).'"' : '').
+					$defaults = array(
+						'width'  => '98%',
+						'height' => '98%'
+					);
+					$args     = array_merge($defaults, $args);
+					$args     = $this->©string->ify_deep($args);
 
-			       ' style="width:'.esc_attr($args['width']).'; height:'.esc_attr($args['height']).';'.
-			       ' padding:1%; background:#CCCCCC; border-radius:3px;"'.
+					return '<iframe type="text/html" frameborder="0"'.
 
-			       (($attrs) ? ' '.$attrs : '').
-			       ' />';
+					       ' src="'.esc_attr($this->yt_playlist_iframe_url($playlist, $args)).'"'.
+
+					       (($classes) ? ' class="'.esc_attr(implode(' ', $classes)).'"' : '').
+
+					       ' style="width:'.esc_attr($args['width']).'; height:'.esc_attr($args['height']).';'.
+					       ' padding:1%; background:#CCCCCC; border-radius:3px;"'.
+
+					       (($attrs) ? ' '.$attrs : '').
+					       '></iframe>';
+				}
 		}
 	}
-}
