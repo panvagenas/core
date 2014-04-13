2e9fe80c222fcf911c12ba34d190f257e798a980
JasWSInc <jason@websharks-inc.com>
2014-04-13 01:21:22 -0800

    Partial rebuild.

--- websharks-core/classes/websharks-core-v000000-dev/dirs.php
+++ websharks-core/classes/websharks-core-v000000-dev/dirs.php
@@ -1112,8 +1112,8 @@ namespace websharks_core_v000000_dev
 
 									if(in_array($_extension, $_strippable_extensions, TRUE) && filesize($_path))
 										{
-											$_no_ws = php_strip_whitespace($_path);
-											file_put_contents($_path, $_no_ws);
+											echo $_path."\n";
+											file_put_contents($_path, php_strip_whitespace($_path));
 										}
 								}
 							$_phar->buildFromDirectory($_temp_dir, $_regex_compressable_extensions);
