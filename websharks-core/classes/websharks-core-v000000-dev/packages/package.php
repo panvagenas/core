ed074d32a28f6c3225a4aed0956a8c87f6c190e6
JasWSInc <jason@websharks-inc.com>
2014-05-23 02:02:14 -0800

    Package manager framework; continuing work on strip down. https://github.com/WebSharks/Core/issues/2

---
+++ websharks-core/classes/websharks-core-v000000-dev/packages/package.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Package.
+ *
+ * Copyright: © 2012 (coded in the USA)
+ * {@link http://www.websharks-inc.com WebSharks™}
+ *
+ * @author JasWSInc
+ * @package WebSharks\Core
+ * @since 140523
+ */
+namespace websharks_core_v000000_dev\packages
+{
+	if(!defined('WPINC'))
+		exit('Do NOT access this file directly: '.basename(__FILE__));
+
+	/**
+	 * Package.
+	 *
+	 * @package WebSharks\Core
+	 * @since 140523
+	 *
+	 * @assert ($GLOBALS[__NAMESPACE__])
+	 */
+	class package extends \websharks_core_v000000_dev\framework
+	{
+		/**
+		 * Constructor.
+		 *
+		 * @param object|array $___instance_config Required at all times.
+		 *    A parent object instance, which contains the parent's `$___instance_config`,
+		 *    or a new `$___instance_config` array.
+		 */
+		public function __construct($___instance_config)
+		{
+			parent::__construct($___instance_config);
+		}
+	}
+}
